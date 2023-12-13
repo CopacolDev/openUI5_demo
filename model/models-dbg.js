8097857d78c88d073ff104ac27686e69a15381a1
integracaoticopacol <111369849+integracaoticopacol@users.noreply.github.com>
2023-12-13 17:48:19 -0300

    VB - commit PIB

--- model/models-dbg.js
+++ model/models-dbg.js
@@ -1,15 +1,16 @@
-"use strict";
-
 sap.ui.define(["sap/ui/model/json/JSONModel", "sap/ui/model/BindingMode", "sap/ui/Device"], function (JSONModel, BindingMode, Device) {
-  "use strict";
+	"use strict";
 
-  var __exports = {
-    createDeviceModel: () => {
-      const oModel = new JSONModel(Device);
-      oModel.setDefaultBindingMode(BindingMode.OneWay);
-      return oModel;
-    }
-  };
-  return __exports;
+	return {
+		createDeviceModel: function () {
+			var oModel = new JSONModel(Device);
+			oModel.setDefaultBindingMode(BindingMode.OneWay);
+			return oModel;
+		},
+        
+        createBlankJSONModel: function () {
+            return new JSONModel();
+      
+        }
+	};
 });
-//# sourceMappingURL=models-dbg.js.map
--- model/models.js
+++ model/models.js
@@ -1,2 +1,2 @@
-"use strict";sap.ui.define(["sap/ui/model/json/JSONModel","sap/ui/model/BindingMode","sap/ui/Device"],function(e,i,n){"use strict";var s={createDeviceModel:()=>{const s=new e(n);s.setDefaultBindingMode(i.OneWay);return s}};return s});
+sap.ui.define(["sap/ui/model/json/JSONModel","sap/ui/model/BindingMode","sap/ui/Device"],function(e,n,i){"use strict";return{createDeviceModel:function(){var t=new e(i);t.setDefaultBindingMode(n.OneWay);return t},createBlankJSONModel:function(){return new e}}});
 //# sourceMappingURL=models.js.map
